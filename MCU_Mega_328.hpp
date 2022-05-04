e46019f94870baab1c58befd044801fa51e7f7b0
R6LHL <69074926+R6LHL@users.noreply.github.com>
2022-05-04 23:08:41 +0300

    Add files via upload

--- MCU_Mega_328.hpp
+++ MCU_Mega_328.hpp
@@ -66,7 +66,7 @@ struct MCU_Mega328
 	};//end General TC control register
 	
 	
-	struct TC0_ // Timer-counter 0
+	struct TC0_ // Timer-counter 0 8bit
 	{
 		//TC0_ flag register
 		struct TIFR0_ : public RegisterBase<0x35, 8, ReadWriteMode>
@@ -109,11 +109,94 @@ struct MCU_Mega328
 		{
 			
 		};//end TC0_ output compare register B
-		
-		
-		
-		
+					
 	}; //end Timer-counter 0
+	
+	struct TC1_ // Timer-counter 1 16bit
+	{
+		//TC1_ control register A
+		struct TCCR1A_ : public RegisterBase<0x80, 8, ReadWriteMode>
+		{
+			
+		}; //end TC1_ control register A
+		
+		//TC1_ control register B
+		struct TCCR1B_ : public RegisterBase<0x81, 8, ReadWriteMode>
+		{
+			
+		}; //end TC1_ control register B
+		
+		//TC1_ control register C
+		struct TCCR1C_ : public RegisterBase<0x82, 8, ReadWriteMode>
+		{
+			
+		}; //end TC1_ control register C
+		
+		//?????????????????Access????????????????????????????????????
+		// TC1_ Counter value low byte
+		struct TCNT1L_ : public RegisterBase<0x84, 8, ReadWriteMode>
+		{
+			
+		};//end TC1_ Counter value low byte
+		
+		// TC1_ Counter value high byte
+		struct TCNT1H_ : public RegisterBase<0x85, 8, ReadWriteMode>
+		{
+			
+		};//end TC1_ Counter value high byte
+		//?????????????????Access????????????????????????????????????
+		
+		//?????????????????Access????????????????????????????????????
+		// TC1_ input capture register 1 low byte
+		struct ICR1L_ : public RegisterBase<0x86, 8, ReadWriteMode>
+		{
+			
+		};//end TC1_ input capture register1 low byte
+		
+		// TC1_ input capture register 1 high byte
+		struct ICR1H_ : public RegisterBase<0x87, 8, ReadWriteMode>
+		{
+			
+		};//end TC1_ input capture register 1 high byte
+		//?????????????????Access????????????????????????????????????
+		
+		//TC1_ Output compare register 1 A low byte
+		struct OCR1AL_ : public RegisterBase<0x88, 8, ReadWriteMode>
+		{
+			
+		};//end TC1_ Output compare register 1 A low byte
+		
+		//TC1_ Output compare register 1 A high byte
+		struct OCR1AH_ : public RegisterBase<0x89, 8, ReadWriteMode>
+		{
+			
+		};//end TC1_ Output compare register 1 A high byte
+		
+		//TC1_ Output compare register 1 B low byte
+		struct OCR1BL_ : public RegisterBase<0x8a, 8, ReadWriteMode>
+		{
+			
+		};//end TC1_ Output compare register 1 B low byte
+		
+		//TC1_ Output compare register 1 B high byte
+		struct OCR1BH_ : public RegisterBase<0x8b, 8, ReadWriteMode>
+		{
+			
+		};//end TC1_ Output compare register 1 B high byte
+		
+		//TC1_ interrupt mask register
+		struct TIMSK1_ : public RegisterBase<0x6f, 8, ReadWriteMode>
+		{
+			
+		};// end TC1_ interrupt mask register
+		
+		//TC1_ interrupt flag register
+		struct TIFR1_ : public RegisterBase<0x36, 8, ReadWriteMode>
+		{
+			
+		};// end TC1_ interrupt flag register
+				
+	};//end  Timer-counter 1 16bit
 	
 };
 
