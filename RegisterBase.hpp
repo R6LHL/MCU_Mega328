5ac0845a01a964e3850a0a5af1abefa6edc37c8d
R6LHL <69074926+R6LHL@users.noreply.github.com>
2022-05-28 21:22:12 +0300

    Упрощение базового класса
    Исходный текст не компилировался под Ардуино, убрал лишние непонятные конструкции - контроль доступа к регистрам, выбор размера регистров

--- RegisterBase.hpp
+++ RegisterBase.hpp
@@ -1,64 +1,22 @@
 #ifndef _REGISTER_BASE_HPP
 #define _REGISTER_BASE_HPP
 
-template<uint32_t address, size_t size, typename AccessMode>
+template<uint32_t address>
 struct RegisterBase
 {
-	static constexpr auto Addr = address;
-	using Type = typename RegisterType<size>::Type;
+	static constexpr auto Addr = address ;
 	
-	//Метод Set будет работать только для регистров, 
-	//в которые можно записать значение
-	__inline template<typename T = AccessMode,
-     class = typename std::enable_if_t<std::is_base_of<WriteMode, T>::value>>
-	 inline static void Set(Type value)
+  inline static void Set(uint8_t value)
   {
-    *reinterpret_cast<volatile Type *>(address) = value ;
+    *reinterpret_cast<volatile uint8_t *>(address) = value ;
   }
   
   //Метод Get возвращает целое значение регистра, 
-  //будет работать только для регистров, которые можно считать
-  __inline template<typename T = AccessMode,
-     class = typename std::enable_if_t<std::is_base_of<ReadMode, T>::value>>
-  inline static Type Get()
+  inline static uint8_t Get()
   {
-    return *reinterpret_cast<volatile Type *>(address) ;
+    return *reinterpret_cast<volatile uint8_t *>(address) ;
   }
 	
 };
-
-//Режим доступа к регистрам
-struct WriteMode {}; 
-struct ReadMode {}; 
-struct ReadWriteMode: public WriteMode, public ReadMode {};
-
-//Тип регистров в зависимости от размера
-
-template <uint32_t size>
-struct RegisterType {} ;
-
-template<>
-struct RegisterType<8>
-{
-  using Type = uint8_t ;
-} ;
-
-template<>
-struct RegisterType<16>
-{
-  using Type = uint16_t ;
-} ;
-
-template<>
-struct RegisterType<32>
-{
-  using Type = uint32_t ;
-} ;
-
-template<>
-struct RegisterType<64>
-{
-  using Type = uint64_t ;
-} ;
 
 #endif //_REGISTER_BASE_HPP
